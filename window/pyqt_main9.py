87367eac8263397cbc5c9410aace2a097c2abc2c
kimjunsu87 <brikant@hanmail.net>
2022-06-27 12:18:29 +0900

    220627 윈도우

---
+++ window/pyqt_main9.py
@@ -0,0 +1,48 @@
+## LED Control UI
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('RPi LED Control')
+        # 윈도우 기본설정
+        self.Geometry(100, 100, 300, 350)
+        self.label = QLabel(self)
+        self.label.setFont(QFont('Arial', 15))
+        self.label.setText('LED Control')
+        self.label.setAlignment(Qt.AlignCenter) # 라벨 정중앙
+
+        self.btnOn = QPushButton('LED ON', self)
+        self.btnOff = QPushButton('LED OFF', self)
+
+        # 시그널
+        self.btnOn.clicked.connect(self.btnOn_Clicked)
+        self.btnOff.clicked.connect(self.btnOff_Clicked)
+
+        self.vbox = QVBoxLayout(self)
+        slef.vbox.setAlignment(Qt.AlignCenter)
+        self.vbox.addWidget(self.label)
+
+        self.hbox = QHBoxLayout(self)
+        self.hbox.addWidget(self.btnOn)
+        self.hbox.addWidget(self.btnoff)
+
+        self.show()
+
+def btnOn_Clicked(self):
+    self.label.setText('LED ON!!')
+
+def btnOff_Clicked(self):
+    self.label.setText('LED OFF')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = MyApp()
+    app.exec_()
